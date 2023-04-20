2f76789c07578264aad2cdd7937488dbde473ed1
Hanna Svietik <hsvietik@gmail.com>
2023-04-20 13:30:51 +0100

    CV page changed

--- js/scriptCV.js
+++ js/scriptCV.js
@@ -9,3 +9,12 @@ const skillsMarkupCV = skillsList.map(
 );
 console.log(skillContainerCV);
 skillContainerCV.insertAdjacentHTML("beforeend", skillsMarkupCV.join(""));
+
+//Education markup main page
+const educationContainerCV = document.querySelector(".cv-education-list");
+const educationMarkup = educationList.map(
+  (education) => `<li>
+<p>${education}</p>
+</li>`
+);
+educationContainerCV.insertAdjacentHTML("beforeend", educationMarkup.join(""));
