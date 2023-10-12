b46ac73ab782aff26904aeccacf05e93cb9ea053
Hanna Svietik <hsvietik@gmail.com>
2023-10-12 16:39:58 +0100

    education and CV changed

--- js/data.js
+++ js/data.js
@@ -1,18 +1,19 @@
 //SKILLS
 export const skillsList = [
-  "Full stack development with core knowledge in HTML, CSS, and JavaScript, and experience using React",
+  "Full stack development with core knowledge in HTML, CSS, and JavaScript, and experience using React, Node.JS",
   "Test Driven Development (TDD)",
   "Familiarity with 3rd party APIs",
   "Version control using Git and GitHub, and experience completing code reviews",
-  "Strong troubleshooting skills",
-  "Experience with Agile methodologies such as Sprints, Kanban, and Stand-ups",
+
+  "Experience with Agile Scrum methodology and scrum events as Sprints and Daily Scrums",
   "Excellent communication, self-learning, and organizational skills",
+  "Natural curiosity and willingness to learn more",
   "Detail-oriented and analytical mindset",
 ];
 //EDUCATION
 export const educationList = [
   "Web Development Bootcamp (JavaScript), The Developer Academy - 09/2022 -12/2022",
-  "Full Stack Developer (HTML, CSS, JavaScript), GoIT, Ukraine - 06/2022 - present time",
+  "Full Stack Developer (HTML, CSS, JavaScript), GoIT, Ukraine - 06/2022 - 10/2023",
   "V. N. Karazin Kharkiv National University, Ukraine, faculty of Economics, specialty administrative management, master's degree - 09/2004 to 02/2006",
   "V. N. Karazin Kharkiv National University, Ukraine, faculty of Psychology, Master's degree (with honours) - 09/2000 to 06/2005",
 ];
